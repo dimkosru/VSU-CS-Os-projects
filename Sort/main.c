22732d230e91a5d53f04b45fbce1e45b24cbe587
dimkosru <dimko@russia.ru>
2012-09-30 17:14:44 +0400

    Buble sort

--- Sort/main.c
+++ Sort/main.c
@@ -3,49 +3,71 @@
 #include <pthread.h>
 #include <time.h>
 
+#define MAX 10
+
 void *print_message_function(void *ptr);
+void *bubbleSort(void *ptr);
 
 int main(void) {
 	pthread_t thread1, thread2;
-	int a[200];
-	int b[200];
+	int a[MAX];
+	int b[MAX];
 	int *message1 = a;
 	int *message2 = b;
-	int iret1, iret2;
 
 	srand(time(NULL ));
 
+    // Заполенение массивов
 	int i = 0;
-	for (i = 0; i < 200; i++) {
-		a[i] = 200 - i;
-		b[i] = 200 - i;
-		//	b[i]=rand()%100;
+	for (i = 0; i < MAX; i++) {
+		a[i] = rand() % MAX;
+		b[i] = rand() % MAX;
+
 	}
-	/* Create independent threads each of which will execute function */
 
-	iret1 = pthread_create(&thread1, NULL, print_message_function,
-			(void*) message1);
-	iret2 = pthread_create(&thread2, NULL, print_message_function,
-			(void*) message2);
-
-	/* Wait till threads are complete before main continues. Unless we  */
-	/* wait we run the risk of executing an exit which will terminate   */
-	/* the process and all threads before the threads have completed.   */
+    // Создание потоков
+	pthread_create(&thread1, NULL, bubbleSort, (void*) message1);
+	pthread_create(&thread2, NULL, bubbleSort, (void*) message2);
 
 	pthread_join(thread1, NULL );
 	pthread_join(thread2, NULL );
 
-	//printf("Thread 1 returns: %d\n", iret1);
-	//printf("Thread 2 returns: %d\n", iret2);
+    // Вывод результата
+	i = 0;
+	for (i = 0; i < MAX; i++) {
+		printf("%d \n", a[i]);
+	}
 	return 0;
 }
 
+/* Тестовая функция для вывода массива */
 void *print_message_function(void *ptr) {
-	int *message;
-	message = (int *) ptr;
+	int *a;
+	a = (int *) ptr;
 
 	int i = 0;
-	for (i = 0; i < 200; i++) {
-		printf("%d \n", message[i]);
+	for (i = 0; i < MAX; i++) {
+		printf("%d \n", a[i]);
 	}
+	return 0;
+}
+
+
+/* Пузырьковая сортировка */
+void *bubbleSort(void *ptr) {
+	int *a;
+	a = (int *) ptr;
+
+	int i, j;
+	int x;
+	for (i = 0; i < MAX; i++) {
+		for (j = MAX - 1; j > i; j--) {
+			if (a[j - 1] > a[j]) {
+				x = a[j - 1];
+				a[j - 1] = a[j];
+				a[j] = x;
+			}
+		}
+	}
+	return 0;
 }
