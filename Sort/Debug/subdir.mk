fbf19f2e68e8d4fbd728f26e89084e4667c04be0
dimkosru <dimko@russia.ru>
2012-09-30 16:50:26 +0400

    Thread test

--- Sort/Debug/subdir.mk
+++ Sort/Debug/subdir.mk
@@ -3,6 +3,9 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../main.o 
+
 C_SRCS += \
 ../main.c 
 
@@ -14,10 +17,10 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-%.o: ../%.c
+main.o: ../main.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -lpthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"main.d" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- Sort/main.c
+++ Sort/main.c
@@ -1,42 +1,51 @@
-/*
- * main.c
- *
- *  Created on: 30.09.2012
- *      Author: dimko
- */
-
 #include <stdio.h>
-#define MAX 10
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+void *print_message_function(void *ptr);
 
 int main(void) {
-	int array[MAX] = { 5, 34, -1, 64, -2, 62, -7, 2, 73, 10 };
-	//int a = 0;
-	//int i = 0;
+	pthread_t thread1, thread2;
+	int a[200];
+	int b[200];
+	int *message1 = a;
+	int *message2 = b;
+	int iret1, iret2;
 
-	/*
-	 while (scan("%d", &a)) {
-	 array[i] = a;
-	 }*/
-	printf("hello");
+	srand(time(NULL ));
 
-	int i=0;
-	for (i = 0; i < MAX; i++) {
-		printf("%d ", array[i]);
+	int i = 0;
+	for (i = 0; i < 200; i++) {
+		a[i] = 200 - i;
+		b[i] = 200 - i;
+		//	b[i]=rand()%100;
 	}
+	/* Create independent threads each of which will execute function */
 
-	for (i = 1; i < MAX; i++) {
-		int key = array[i];
-		int j = i - 1;
-		while (j >= 0 && array[j] > key) {
-			array[j + 1] = array[j];
-			j = j - 1;
-		}
-		array[j + 1] = key;
-	}
+	iret1 = pthread_create(&thread1, NULL, print_message_function,
+			(void*) message1);
+	iret2 = pthread_create(&thread2, NULL, print_message_function,
+			(void*) message2);
 
-	printf("\n");
-	for (i = 0; i < MAX; i++) {
-		printf("%d ", array[i]);
-	}
+	/* Wait till threads are complete before main continues. Unless we  */
+	/* wait we run the risk of executing an exit which will terminate   */
+	/* the process and all threads before the threads have completed.   */
+
+	pthread_join(thread1, NULL );
+	pthread_join(thread2, NULL );
+
+	//printf("Thread 1 returns: %d\n", iret1);
+	//printf("Thread 2 returns: %d\n", iret2);
 	return 0;
+}
+
+void *print_message_function(void *ptr) {
+	int *message;
+	message = (int *) ptr;
+
+	int i = 0;
+	for (i = 0; i < 200; i++) {
+		printf("%d \n", message[i]);
+	}
 }
